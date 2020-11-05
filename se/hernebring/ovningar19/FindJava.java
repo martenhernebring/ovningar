b7645891712b7761e3fa3a8d253f09f666429bc1
Marten Hernebring <marten.hernebring@gmail.com>
2020-11-05 09:51:32 +0100

    Changed FindJava and created ovningar 20

--- se/hernebring/ovningar19/FindJava.java
+++ se/hernebring/ovningar19/FindJava.java
@@ -3,18 +3,19 @@ package se.hernebring.ovningar19;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FindJava {
     public static void main(String[] args) {
-        if(args == null || args.length != 1 ){
-            System.err.println("Usage: FindJava directory");
+        if(args == null || args.length != 2 ){
+            System.err.println("Usage: FindJava directory ending");
         } else{
             String catalog = args[0];
-            try(Stream<Path> walk = Files.walk(Paths.get(catalog))){
-                walk.filter(p -> p.toString()
-                    .endsWith(".java"))
+            final String ending = args[1];
+            try(Stream<Path> walk = Files.walk(Path.of(catalog))){
+                walk.filter(p -> p.toString().endsWith(ending))
+                    .map(Path::toAbsolutePath)
+                    .map(Path::normalize)
                     .forEach(System.out::println);
             } catch (IOException ioe){
                 ioe.printStackTrace();
